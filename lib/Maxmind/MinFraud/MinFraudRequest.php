a93b238760a89018c90629c221a0c6add98f79d2
Dmitry Vrublevskis <d.vrublevskis@gmail.com>
2014-02-14 17:48:01 +0200

    Add Shipping address to request

--- lib/Maxmind/MinFraud/MinFraudRequest.php
+++ lib/Maxmind/MinFraud/MinFraudRequest.php
@@ -40,4 +40,37 @@ class MinFraudRequest
     {
         return $this->requestData;
     }
+
+    /**
+     * @param array $data
+     */
+    public function setShippingAddress(array $data)
+    {
+        $this->setRequestDataField('shipAddr', $data);
+        $this->setRequestDataField('shipCity', $data);
+        $this->setRequestDataField('shipRegion', $data);
+        $this->setRequestDataField('shipPostal', $data);
+        $this->setRequestDataField('shipCountry', $data);
+    }
+
+    /**
+     * @param $field
+     * @param array $data
+     * @param string $default
+     */
+    private function setRequestDataField($field, array $data, $default = '')
+    {
+        $this->requestData[$field] = $this->getDataValue($data, $field, $default);
+    }
+
+    /**
+     * @param array $data
+     * @param $key
+     * @param string $default
+     * @return mixed
+     */
+    private function getDataValue(array $data, $key, $default = '')
+    {
+        return isset($data[$key]) ? $data[$key] : $default;
+    }
 } 
--- tests/Maxmind/MinFraud/MinFraudRequestTest.php
+++ tests/Maxmind/MinFraud/MinFraudRequestTest.php
@@ -7,21 +7,35 @@ use Maxmind\MinFraud\MinFraudRequest;
 class MinFraudRequestTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var array
+     */
+    private $requiredData = array(
+        'i'           => '127.0.0.1',
+        'city'        => 'City',
+        'region'      => 'Region',
+        'postal'      => '1111',
+        'country'     => 'NA',
+        'license_key' => '11'
+    );
+
+    /**
+     * @var MinFraudRequest
+     */
+    private $request;
+
+    protected function setUp()
+    {
+        $this->request = new MinFraudRequest($this->requiredData);
+    }
+
+    /**
      * @dataProvider getRequiredFieldForDisable
      * @expectedException \Maxmind\MinFraud\exceptions\MissingRequiredFieldException
      */
     public function testIfRequiredFieldMissedExceptionThrown($requiredFieldForDisable)
     {
-        $requiredData = array(
-            'i'           => '127.0.0.1',
-            'city'        => 'City',
-            'region'      => 'Region',
-            'postal'      => '1111',
-            'country'     => 'NA',
-            'license_key' => '11'
-        );
-        unset($requiredData[$requiredFieldForDisable]);
-        new MinFraudRequest($requiredData);
+        unset($this->requiredData[$requiredFieldForDisable]);
+        new MinFraudRequest($this->requiredData);
     }
 
     public function getRequiredFieldForDisable()
@@ -36,19 +50,52 @@ class MinFraudRequestTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-    public function testRequestDataSetWithReuiredData()
+    public function testRequestDataSetWithRequiredData()
     {
-        $requiredData = array(
+        $this->assertEquals($this->requiredData, $this->request->getRequestData());
+    }
+
+    public function testSetShippingAddressEmpty()
+    {
+        $this->request->setShippingAddress(array());
+
+        $this->assertEquals(array(
             'i'           => '127.0.0.1',
             'city'        => 'City',
             'region'      => 'Region',
             'postal'      => '1111',
             'country'     => 'NA',
-            'license_key' => '11'
-        );
+            'license_key' => '11',
+            'shipAddr'    => '',
+            'shipCity'    => '',
+            'shipRegion'  => '',
+            'shipPostal'  => '',
+            'shipCountry' => '',
+        ), $this->request->getRequestData());
+    }
+    public function testSetShippingAddress()
+    {
+        $this->request->setShippingAddress(array(
+            'shipAddr'    => '1',
+            'shipCity'    => '2',
+            'shipRegion'  => '3',
+            'shipPostal'  => '4',
+            'shipCountry' => '5',
+        ));
 
-        $request = new MinFraudRequest($requiredData);
-        $this->assertEquals($requiredData, $request->getRequestData());
+        $this->assertEquals(array(
+            'i'           => '127.0.0.1',
+            'city'        => 'City',
+            'region'      => 'Region',
+            'postal'      => '1111',
+            'country'     => 'NA',
+            'license_key' => '11',
+            'shipAddr'    => '1',
+            'shipCity'    => '2',
+            'shipRegion'  => '3',
+            'shipPostal'  => '4',
+            'shipCountry' => '5',
+        ), $this->request->getRequestData());
     }
 }
  
